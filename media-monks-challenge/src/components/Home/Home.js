5338f02c03247fdc1667f15b3958259299679e88
Dario Jejer <64561289+DarioJejer@users.noreply.github.com>
2021-11-20 11:26:43 -0300

    Added skeletons when loading characters on Home

--- media-monks-challenge/src/components/Home/Home.js
+++ media-monks-challenge/src/components/Home/Home.js
@@ -3,33 +3,58 @@ import React, { useEffect, useState } from "react";
 import { CharacterCard } from "../Character/CharacterCard";
 import { SearchBar } from "./SearchBar";
 import { useSelector } from "react-redux";
-import { Button } from "@mui/material";
+import { Button, Skeleton } from "@mui/material";
 import { Box } from "@mui/system";
 
 export const Home = () => {
   
   const [displayedCharacters, setDisplayedCharacters] = useState([])
+  const [loading, setLoading] = useState(true)
   const [page, setPage] = useState(1)
   const characters = useSelector(state => state.characters)
 
   useEffect(() => {
-    setDisplayedCharacters(characters.slice(0,100))
-  }, [characters])
+      setDisplayedCharacters(characters.slice(0,100))
+      if(characters.length > 0){
+        setLoading(false);
+      }
+    }, [characters])
 
   const handleClick = () => {
     setDisplayedCharacters([...displayedCharacters, ...characters.slice(page*100,(page+1)*100)])
     setPage(page+1);
   }
 
+  const skeletons = [...Array(20).keys()];
+
   return (
     <>  
       <SearchBar/>
-      <div className="characters-grid">
-        {displayedCharacters.map( (character, i) => <CharacterCard key={i} character={character} />)}
-      </div>
-      <Box sx={{ justifyContent: 'center', display: "flex",  p: 2 }}>
-        <Button variant="contained" onClick={handleClick}>Load mode characters</Button>
-      </Box>
+      {loading ? (
+        <div className="characters-grid">
+          {skeletons.map( c => {
+            return (
+              <Box sx={{marginY:"30px"}}>
+                <Skeleton variant="rectangular" width={300} height={300} />
+                <Skeleton width="60%" height={50} />
+                <Skeleton />
+                <Skeleton />
+                <Skeleton />
+                <Skeleton variant="circular" width={40} height={40}/>
+              </Box>
+            )
+          })}
+        </div>
+      ) : (
+        <>
+          <div className="characters-grid">
+            {displayedCharacters.map( (character, i) => <CharacterCard key={i} character={character} />)}
+          </div>
+          <Box sx={{ justifyContent: 'center', display: "flex",  p: 2 }}>
+            <Button variant="contained" onClick={handleClick}>Load mode characters</Button>
+          </Box>
+        </>
+      )}
     </>
   );
 };
