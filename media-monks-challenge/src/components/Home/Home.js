8fbad70c0262e6dc842459f7f4cda93e8bbffbca
Dario Jejer <64561289+DarioJejer@users.noreply.github.com>
2021-11-20 00:24:11 -0300

    Implemented pages on Home

--- media-monks-challenge/src/components/Home/Home.js
+++ media-monks-challenge/src/components/Home/Home.js
@@ -1,19 +1,35 @@
 import "./Home.css";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { CharacterCard } from "../Character/CharacterCard";
 import { SearchBar } from "./SearchBar";
 import { useSelector } from "react-redux";
+import { Button } from "@mui/material";
+import { Box } from "@mui/system";
 
 export const Home = () => {
   
+  const [displayedCharacters, setDisplayedCharacters] = useState([])
+  const [page, setPage] = useState(1)
   const characters = useSelector(state => state.characters)
+
+  useEffect(() => {
+    setDisplayedCharacters(characters.slice(0,100))
+  }, [characters])
+
+  const handleClick = () => {
+    setDisplayedCharacters([...displayedCharacters, ...characters.slice(page*100,(page+1)*100)])
+    setPage(page+1);
+  }
 
   return (
     <>  
       <SearchBar/>
       <div className="characters-grid">
-        {characters.map( (character, i) => <CharacterCard key={i} character={character} />)}
+        {displayedCharacters.map( (character, i) => <CharacterCard key={i} character={character} />)}
       </div>
+      <Box sx={{ justifyContent: 'center', display: "flex",  p: 2 }}>
+        <Button variant="contained" onClick={handleClick}>Load mode characters</Button>
+      </Box>
     </>
   );
 };
