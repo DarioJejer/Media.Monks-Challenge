db6da78e9235d48a432857d63738e9d43450107e
Dario Jejer <64561289+DarioJejer@users.noreply.github.com>
2021-11-20 11:45:03 -0300

    Added Bookmark icon on CharacterCard

--- media-monks-challenge/src/components/Character/CharacterCard.js
+++ media-monks-challenge/src/components/Character/CharacterCard.js
@@ -3,20 +3,26 @@ import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
-import { CardActionArea } from '@mui/material';
+import { CardActionArea, CardActions, IconButton } from '@mui/material';
 import { useHistory } from "react-router";
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import { Box } from "@mui/system";
+import BookmarkBorderIcon from '@mui/icons-material/BookmarkBorder';
 
 export const CharacterCard = ({character}) => {
     const { push } = useHistory();
 
-    const handleClick = () => {
+    const handleDetails = () => {
+        push(`/character/${character.id}`)
+    };
+    const handleFavorite = () => {
         push(`/character/${character.id}`)
     };
 
     return (        
         <div>
-            <Card sx={{ width: 300, height: 500, margin: 2}}>
-                <CardActionArea onClick={handleClick}>
+            <Card sx={{ width: 300, height: 530, margin: 2}}>
+                <CardActionArea onClick={handleDetails}>
                     <CardMedia
                     component="img"
                     height="300"
@@ -25,13 +31,34 @@ export const CharacterCard = ({character}) => {
                     />
                     <CardContent>
                         <Typography gutterBottom variant="h5" component="div">
-                            {character.name}
+                            <Box 
+                                overflow="hidden"            
+                                height={30}
+                            >
+                                {character.name}
+                            </Box>
                         </Typography>
-                        <Typography variant="body2" color="text.secondary">
+                        <Typography noWrap variant="body2" color="text.secondary">
+                            <Box
+                                fontSize="h5.fontSize"
+                                component="div" 
+                                overflow="hidden"            
+                                whiteSpace="pre-line"
+                                textOverflow="ellipsis"
+                                height={120}
+                            >
                             {character.description}
+                            </Box>
                         </Typography>
                     </CardContent>
                 </CardActionArea>
+                <CardActions>
+                    <Box marginTop="-20px">
+                        <IconButton aria-label="add to favorites" onClick={handleFavorite}>
+                            <BookmarkBorderIcon />
+                        </IconButton>
+                    </Box>
+                </CardActions>
             </Card>
         </div>
     )
